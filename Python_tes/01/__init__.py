a1f1bda1a0d61335fad1914374a0031f56235dba
tanxvyang <your_email@1173195180@qq.com>
2018-04-16 10:23:30 +0800

    4.14

---
+++ Python_tes/01/__init__.py
@@ -0,0 +1,4 @@
+"hello world"
+print("hello  world");
+name=input("what is you name:")
+print("hello"+name+"!" )
---
+++ src/Hello.py
@@ -0,0 +1,5 @@
+'''
+Created on 2018年4月
+@author: MR。tan
+'''
+print("hello world!")
