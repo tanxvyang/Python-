2f58213bca41a263ada1f09035aedd42f9f03385
谭旭洋 <tanxvyang@outlook.com>
2019-04-12 17:54:20 +0800

    a'

---
+++ src/bridTest01.py
@@ -0,0 +1,24 @@
+#注释一
+#注释二
+print ("hello,python!") #第二个注释
+"""
+第二注释
+"""
+"sdffdsgdgsdgfsdgf"
+
+'''
+afdaafd
+'''
+
+str='Runoob'
+print(str)
+print(str[0:-1])
+print(str[0])
+print(str[2:5])
+print(str[2:])
+print(str * 2)
+print(str + '你好!')
+print('---------------------------------')
+print('hello\nrunoob')
+print(r'hello\nrunoob')
+print('hello  runoob') 
