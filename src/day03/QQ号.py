6ef5a313839f8bdd7fc7c3a4952952c0c6a944c6
谭旭洋 <tanxvyang@outlook.com>
2019-04-15 00:00:09 +0800

    day03

---
+++ src/day03/QQ号.py
@@ -0,0 +1,7 @@
+#定义一个变量记录QQ号码
+qq_number = "1234567"
+#定义一个变量记录QQ密码
+qq_password = "123"
+
+qq_number  #在使用解释器中执行python时
+print(qq_number)
---
+++ src/day03/超市买苹果.py
@@ -0,0 +1,9 @@
+#定义单价
+price = 8.5
+#挑选苹果
+weight = 7.5
+#计算金额
+money = weight*price
+#只要买苹果就返五元
+money = money - 5
+print(money)
